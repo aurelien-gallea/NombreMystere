747967e266e5d55eea59523bf616a5298f3a75db
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-06-13 15:03:33 +0200

    ajout du mini jeu

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿const int mysteryNumber = 7;
+var playerNumber = 0;
+do
+{
+    Console.Write("Devinez le nombre entre 1 et 10 : ");
+    playerNumber = int.Parse(Console.ReadLine());
+    Console.WriteLine(playerNumber < mysteryNumber ? $"Le chiffre mystère est plus grand que {playerNumber}" : $"Le chiffre mystère est plus petit que {playerNumber}");   
+} while (playerNumber != mysteryNumber);
+
+Console.WriteLine("Bravo vous avez gagné !");
