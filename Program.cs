e143739427e4b939ed32bbef02bdfa597d3f9a95
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-06-13 22:38:46 +0200

    ajout d'un try catch pour éviter si l'utilisateur saisie une string au lieu d'un nombre

--- Program.cs
+++ Program.cs
@@ -10,18 +10,24 @@ void play() {
 
     do
     {
-        Console.Write("Devinez le nombre entre 1 et 10 : ");
-        playerNumber = int.Parse(Console.ReadLine());
-        count++;
-
-        if (playerNumber < mysteryNumber) {
-            Console.WriteLine($"Le chiffre mystère est plus grand que {playerNumber}");
-        } else if (playerNumber > mysteryNumber) {
-            Console.WriteLine($"Le chiffre mystère est plus petit que {playerNumber}");   
-        } else {
-            Console.WriteLine($"Bravo \"{name}\" vous avez gagné en {count} coup(s) !");
-        }
+        Console.Write("------------------- Devinez le nombre entre 1 et 10 : ");
         
+        try{
+
+            playerNumber = int.Parse(Console.ReadLine());
+            count++;
+
+            if (playerNumber < mysteryNumber) {
+                        Console.WriteLine($"Le chiffre mystère est plus grand que {playerNumber}");
+                    } else if (playerNumber > mysteryNumber) {
+                        Console.WriteLine($"Le chiffre mystère est plus petit que {playerNumber}");   
+                    } else {
+                        Console.WriteLine($"Bravo \"{name}\" vous avez gagné en {count} coup(s) !");
+                    }
+        } catch {
+            Console.WriteLine("Entrez un Nombre entre 1 et 10 !");
+        }
+    
     } while (playerNumber != mysteryNumber);
 
     Console.Write("Voulez-vous rejouer ? o / n : ");
